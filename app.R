3de40052ab154e17bc85a33cfaf44c1209aaa15e
Will P Ball <61555378+will-ball@users.noreply.github.com>
2022-04-07 16:20:43 +0100

    updated app.r

--- app.R
+++ app.R
@@ -16,27 +16,8 @@ theme_update(panel.grid.major.y = element_line(),
              panel.grid.major.x = element_line(),
              plot.title.position = "plot")
 
-# Domain selector
-vars <- c(
-  "Overall" = "rank_decile",
-  "Income" = "inc_rank_decile",
-  "Employment" = "emp_rank_decile",
-  "Health" = "hlth_rank_decile",
-  "Education" = "edu_rank_decile",
-  "Geographic Access" = "g_acc_rank_decile",
-  "Crime" = "crime_rank_decile",
-  "Housing" = "house_rank_decile")
-
-# Palette
-mypalette <- colorFactor(palette = "viridis", domain = sf_datazones$rank_decile, na.color = "transparent")
-
-# Labels
-mylabels <- paste0(
-  "Datazone: ", sf_datazones$dz_name,"<br/>",
-  "Council Area: ", sf_datazones$la_name,"<br/>",
-  "Decile: ", sf_datazones$rank_decile, # Should be reactive
-  sep = "") %>% 
-  lapply(htmltools::HTML)
+# Variables
+vars <- c("Overall", "Income", "Employment", "Health", "Education", "Geographic Access", "Crime", "Housing")
 
 ##########
 ### UI ###
@@ -60,8 +41,7 @@ ui <- fluidPage(
       tabsetPanel(
     
       tabPanel("Map",leafletOutput("map", height = "80vh")),            # map is the choropleth
-      tabPanel("Comparing SIMD Ranks", plotOutput("cross")),            # cross is the geom_point plot comparing ranks
-      tabPanel("Data", DT::dataTableOutput("table"))                                               
+      tabPanel("Comparing SIMD Ranks", plotOutput("cross"))            # cross is the geom_point plot comparing ranks
       )
       
     )
@@ -74,26 +54,36 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
   
-#  domain <- reactive({
-#    if ("Overall" %in% input$domain)           return(sf_datazones$rank_decile)
-#    if ("Income" %in% input$domain)            return(sf_datazones$inc_rank_decile)
-#    if ("Employment" %in% input$domain)        return(sf_datazones$emp_rank_decile)
-#    if ("Health" %in% input$domain)            return(sf_datazones$hlth_rank_decile)        # This should assign the correct variable values to display on choropleth map & y axis of plot
-#    if ("Education" %in% input$domain)         return(sf_datazones$edu_rank_decile)
-#    if ("Geographic Access" %in% input$domain) return(sf_datazones$g_acc_rank_decile)
-#    if ("Crime" %in% input$domain)             return(sf_datazones$crime_rank_decile)
-#    if ("Housing" %in% input$domain)           return(sf_datazones$house_rank_decile)
-#  })
-
   # Leaflet Map  
   output$map <- renderLeaflet({
+    
+    domain <- switch(input$domain,
+      "Overall" = sf_datazones$rank_decile,
+      "Income" = sf_datazones$inc_rank_decile,
+      "Employment" = sf_datazones$emp_rank_decile,
+      "Health" = sf_datazones$hlth_rank_decile,
+      "Education" = sf_datazones$edu_rank_decile,
+      "Geographic Access" = sf_datazones$g_acc_rank_decile,
+      "Crime" = sf_datazones$crime_rank_decile,
+      "Housing" = sf_datazones$house_rank_decile)
+    
+    mypalette <- colorFactor(palette = "viridis", domain = domain, na.color = "transparent")
+    
+    mylabels <- paste0(
+      "Datazone: ", sf_datazones$dz_name,"<br/>",
+      "Council Area: ", sf_datazones$la_name,"<br/>",
+      {input$domain}," Decile: ", domain, # Should be reactive
+      sep = "") %>% 
+      lapply(htmltools::HTML)
+    
+    
     leaflet(sf_datazones) %>% 
     addTiles() %>% 
     setView( lng = -4.182,
-             lat = 56.817,
-             zoom = 7) %>% 
+             lat = 57.750,
+             zoom = 6) %>% 
     addPolygons(
-      fillColor = ~mypalette(rank_decile), # Should point to variable from domain selection above
+      fillColor = ~mypalette(domain), # Should point to variable from domain selection above
       weight = .5,
       opacity = 1,
       color = "grey",
@@ -107,39 +97,33 @@ server <- function(input, output, session){
       pal = mypalette,
       values = ~rank_decile,
       opacity = .7,
-      title = "Overall\nDecile", # Would be nice to add reactive text to reflect domain chosen
+      title = paste0({input$domain},"\nDecile"), # Would be nice to add reactive text to reflect domain chosen
       position = "bottomright")
   })
   
   # Rank Comparison Plot
   output$cross <- renderPlot({
-    ggplot(data = sf_datazones, aes(x = rank, y = inc_rank)) +  # y axis should be populated from domain input selection
+    
+    domain <- switch(input$domain,
+                     "Overall" = sf_datazones$rank,
+                     "Income" = sf_datazones$inc_rank,
+                     "Employment" = sf_datazones$emp_rank,
+                     "Health" = sf_datazones$hlth_rank,
+                     "Education" = sf_datazones$edu_rank,
+                     "Geographic Access" = sf_datazones$g_acc_rank,
+                     "Crime" = sf_datazones$crime_rank,
+                     "Housing" = sf_datazones$house_rank)
+    
+    ggplot(data = sf_datazones, aes(x = rank, y = domain)) +  # y axis should be populated from domain input selection
       geom_point(size = 1.5, alpha = .5, colour = "dodgerblue4") +
       scale_x_continuous(limits = c(1,7000), breaks = seq(0,7000,by=1000), labels = comma) +
       scale_y_continuous(limits = c(1,7000), breaks = seq(0,7000,by=1000), labels = comma) +
-      labs(x = "Overall Rank", y = "Income Rank", subtitle = "Areas are ranked from 1 (Most Deprived) to 6,976 (Least Deprived)") # y label should reflect chosen domain automatically
+      labs(x = "Overall Rank", y = paste0({input$domain}, " Rank"), subtitle = "Areas are ranked from 1 (Most Deprived) to 6,976 (Least Deprived)") # y label should reflect chosen domain automatically
       },
     res = 150,
     height = function() {
       session$clientData$output_cross_width * .9
     } )
-  
-  # Table
-  output$table <- renderDataTable({
-    sf_datazones %>%
-      st_drop_geometry() %>% 
-      rename("Datazone" = "dz_name",
-             "Council Area" = "la_name",
-             "Income Decile" = "inc_rank_decile",
-             "Employment Decile" = "emp_rank_decile",
-             "Health Decile" = "hlth_rank_decile",
-             "Education Decile" = "edu_rank_decile",
-             "Access Decile" = "g_acc_rank_decile",
-             "Crime Decile" = "crime_rank_decile",
-             "Housing Decile" = "house_rank_decile",
-             "Overall Decile" = "rank_decile") %>%
-      select(Datazone, 'Council Area', 'Overall Decile', 'Income Decile')  # 4th variable should change based on selected domain input
-  })
   
 }
 
